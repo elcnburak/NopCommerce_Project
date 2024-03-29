0c19bee4e132bebca095e4727b19e4466c2c3887
elcnburak <elcnburak@hotmail.com>
2024-03-29 22:55:58 +0000

    Log changed

--- src/US_505/US_505.java
+++ src/US_505/US_505.java
@@ -1,7 +1,12 @@
 package US_505;
 
 import Utility.BaseDriver;
+import org.testng.annotations.Test;
 
 public class US_505 extends BaseDriver {
 
+    @Test
+    public void Login(){
+        System.out.println("deneme");
+    }
 }
