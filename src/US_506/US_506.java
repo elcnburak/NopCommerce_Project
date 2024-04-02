5f0da3f1035d2e82316bf2181478d79e4b38d67c
elcnburak <elcnburak@hotmail.com>
2024-04-02 23:25:55 +0100

    Random Register and Login added

--- src/US_506/US_506.java
+++ src/US_506/US_506.java
@@ -1,4 +1,18 @@
 package US_506;
 
-public class US_506 {
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class US_506 extends BaseDriver {
+
+    @Test
+    public void US_506TestCase() {
+
+        US_506_Elements elements= new US_506_Elements();
+
+        elements.giftCardLink.click();
+
+    }
+
+
 }
--- src/US_506/US_506_Elements.java
+++ src/US_506/US_506_Elements.java
@@ -1,4 +1,16 @@
 package US_506;
 
+import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class US_506_Elements {
+
+    @FindBy(xpath="//a[@href='/gift-cards']")
+    public WebElement giftCardLink;
+
+    public US_506_Elements() {
+        PageFactory.initElements(BaseDriver.driver, this);
+    }
 }
