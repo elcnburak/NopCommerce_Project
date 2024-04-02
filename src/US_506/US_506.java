6a289e311ad1bfd51d570aa0292550fe48a28118
elcnburak <elcnburak@hotmail.com>
2024-04-03 00:00:32 +0100

    US_506 completed.

--- src/US_506/US_506.java
+++ src/US_506/US_506.java
@@ -1,7 +1,14 @@
 package US_506;
 
 import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class US_506 extends BaseDriver {
 
@@ -10,7 +17,21 @@ public class US_506 extends BaseDriver {
 
         US_506_Elements elements= new US_506_Elements();
 
-        elements.giftCardLink.click();
+        elements.giftCardLink.click();// Menüden giftcard link tıklandı
+        elements.giftCard.click();// gift card seçildi
+        elements.recipientName.sendKeys("Elcin");// recipient name gönderildi
+        elements.yourName.clear();// your name temizlendi
+        elements.yourName.sendKeys("Elcin");// name gönderildi
+        elements.message.sendKeys("Gift hediye kartı istiyorum");// mesaj yazıldı
+        elements.AddToCartButon.click();// sepete eklendi
+
+        // Doğrulama
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("content")));
+        String actualMessage = message.getText();
+        String expectedMessage = "The product has been added to your shopping cart";
+        Assert.assertTrue(actualMessage.contains(expectedMessage), "Mesaj görünmedi veya beklenen metni içermiyor.");
+
 
     }
 
--- src/US_506/US_506_Elements.java
+++ src/US_506/US_506_Elements.java
@@ -10,6 +10,25 @@ public class US_506_Elements {
     @FindBy(xpath="//a[@href='/gift-cards']")
     public WebElement giftCardLink;
 
+    @FindBy(xpath = "//img[@alt='Picture of $100 Physical Gift Card']")
+    public WebElement giftCard;
+
+    @FindBy(xpath ="//input[@class='recipient-name']")
+    public WebElement recipientName;
+
+    @FindBy(xpath = "//input[contains(@class, 'sender-name')]")
+    public WebElement yourName;
+
+    @FindBy(xpath = "//textarea[@class='message']")
+    public WebElement message;
+
+    @FindBy(xpath = "//button[@id='add-to-cart-button-45']")
+    public WebElement AddToCartButon;
+
+    @FindBy(xpath = "//p[@class='content']")
+    public WebElement addedMessage;
+
+
     public US_506_Elements() {
         PageFactory.initElements(BaseDriver.driver, this);
     }
