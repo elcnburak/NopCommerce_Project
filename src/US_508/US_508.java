308d24019050fb2086f9eae5e723ebe9e102d0aa
gu1mira <sidagulova@gmail.com>
2024-04-05 13:07:42 +0300

    US_508 is completed.

--- src/US_508/US_508.java
+++ src/US_508/US_508.java
@@ -1,4 +1,20 @@
 package US_508;
 
-public class US_508 {
+import Utility.BaseDriver;
+import Utility.Tools;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class US_508 extends BaseDriver {
+    @Test (priority = 2, groups ={"UITesting", "Search", "Smoke", "Regression"})
+    @Parameters("searchItem")
+    public void ParameterizedSearchProcessTest(String searchItem){
+        US_508_Elements elems=new US_508_Elements();
+        Assert.assertTrue(elems.searchField.isDisplayed(), "The search box is not displayed");
+        Tools.mySendKeys(elems.searchField, searchItem+ Keys.ENTER);
+        Assert.assertTrue(elems.productTitleAfterSearch.getText().equalsIgnoreCase("Adobe Photoshop CS4"));
+
+    }
 }
--- src/US_508/US_508_Elements.java
+++ src/US_508/US_508_Elements.java
@@ -1,4 +1,23 @@
 package US_508;
 
-public class US_508_Elements {
+import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class US_508_Elements extends BaseDriver {
+    US_508_Elements() {PageFactory.initElements(BaseDriver.driver,this);}
+
+    @FindBy(xpath = "//*[@placeholder='Search store']")
+    public WebElement searchField;
+
+    @FindBy(xpath = "//*[@class='product-title']/a")
+    public WebElement productTitleAfterSearch;
+
+
+
+
+
+
+
 }
