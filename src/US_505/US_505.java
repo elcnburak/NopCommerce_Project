97bc7ffe395afd650bbbf8d6a7a1a863f1ddc256
elcnburak <elcnburak@hotmail.com>
2024-04-07 00:13:24 +0100

    US_505 main and submenu added.

--- src/US_505/US_505.java
+++ src/US_505/US_505.java
@@ -1,12 +1,48 @@
 package US_505;
 
-import Utility.BaseDriver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import Utility.BaseDriver;
+import java.util.List;
 
 public class US_505 extends BaseDriver {
 
     @Test
-    public void Login(){
-        System.out.println("deneme");
+    public void TabMenuControl() {
+        US_505_Elements elements = new US_505_Elements();
+        List<WebElement> mainMenuItems = elements.mainMenuItems;
+
+        System.out.println("Ana Menü İsimleri:");
+        for (WebElement menuItem : mainMenuItems) {
+            System.out.println(menuItem.getText().trim());
+        }
     }
-}
+
+    @Test
+    public void SubMenuControl() {
+        US_505_Elements elements = new US_505_Elements();
+        elements.computerLink.click();
+
+        List<WebElement> subMenuItems = elements.subMenuItems;
+
+        if (subMenuItems.isEmpty()) {
+            System.out.println("Alt menü öğeleri bulunamadı.");
+        } else {
+            System.out.println("Alt Menü İsimleri:");
+            for (WebElement menuItem : subMenuItems) {
+                System.out.println(menuItem.getText().trim());
+            }
+        }
+    }
+
+    @Test
+    @Parameters("searchText")
+    public void testRandomProductSelection(String searchText) {
+
+        }
+    }
+
+
--- src/US_505/US_505_Elements.java
+++ src/US_505/US_505_Elements.java
@@ -1,4 +1,32 @@
 package US_505;
 
+import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class US_505_Elements {
+
+
+    @FindBy(xpath="//a[@href='/computers']")
+    public WebElement computerLink;
+
+    @FindBy(css = ".top-menu.notmobile > li > a")
+    public List<WebElement> mainMenuItems;
+
+    @FindBy(xpath = "//a[@href='/computers']/following-sibling::ul[@class='sublist']//a")
+    public List<WebElement> subMenuItems;
+    @FindBy(xpath ="//input[@id='small-searchterms']")
+    public WebElement search;
+
+
+
+
+    public US_505_Elements() {
+        PageFactory.initElements(BaseDriver.driver, this);
+    }
+
+
 }
